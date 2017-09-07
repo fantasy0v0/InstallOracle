0622e699b70bcb169b7cafe596fbb6b5e4745e5b
Fan <xiaofanms@outlook.com>
2017-09-07 22:19:00 +0800

    添加环境变量设置脚本。

---
+++ set-env.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+user=`whoami`
+if [ "oracle" != $user ];then
+    echo "The current user is not oracle."
+    exit
+fi
+
+
+echo "" >> ~/.bash_profile
+echo "export ORACLE_HOSTNAME=ORCL" >> ~/.bash_profile
+echo "export ORACLE_BASE=/u01/app/oracle" >> ~/.bash_profile
+echo "export ORACLE_HOME=\$ORACLE_BASE/product/11.2.0/dbhoe_1" >> ~/.bash_profile
+echo "export ORACLE_SID=ORCL" >> ~/.bash_profile
+echo "export PATH=.:\$ORACLE_HOME/bin:\$ORACLE_HOME/OPatch:\$ORACLE_HOME/jdk/bin:\$PATH" >> ~/.bash_profile
+echo "export LC_ALL=\"en_US\"" >> ~/.bash_profile
+echo "export LANG=\"en_US\"" >> ~/.bash_profile
+echo "export NSL_LANG=\"AMERICAN_AMERICA.ZHS16GBK\"" >> ~/.bash_profile
+echo "export NLS_DATE_FORMAT=\"YYYY-MM-DD HH24:MI:SS\"" >> ~/.bash_profile
+echo "export DISTRIB=/u01/database" >> ~/.bash_profile
+echo "" >> ~/.bash_profile
+
+. ~/.bash_profile
