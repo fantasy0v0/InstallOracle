e9eca70f0d096e47ca5eca039023e154d4eaa7fd
Fan <xiaofanms@outlook.com>
2017-09-10 11:19:39 +0800

    提取变量，降低修改难度。

--- set-env.sh
+++ set-env.sh
@@ -5,18 +5,19 @@ if [ "oracle" != $user ];then
     exit
 fi
 
+filePath=~/.bash_profile
 
-echo "" >> ~/.bash_profile
-echo "export ORACLE_HOSTNAME=ORCL" >> ~/.bash_profile
-echo "export ORACLE_BASE=/u01/app/oracle" >> ~/.bash_profile
-echo "export ORACLE_HOME=\$ORACLE_BASE/product/11.2.0/dbhome_1" >> ~/.bash_profile
-echo "export ORACLE_SID=ORCL" >> ~/.bash_profile
-echo "export PATH=.:\$ORACLE_HOME/bin:\$ORACLE_HOME/OPatch:\$ORACLE_HOME/jdk/bin:\$PATH" >> ~/.bash_profile
-echo "export LC_ALL=\"en_US\"" >> ~/.bash_profile
-echo "export LANG=\"en_US\"" >> ~/.bash_profile
-echo "export NSL_LANG=\"AMERICAN_AMERICA.ZHS16GBK\"" >> ~/.bash_profile
-echo "export NLS_DATE_FORMAT=\"YYYY-MM-DD HH24:MI:SS\"" >> ~/.bash_profile
-echo "export DISTRIB=/u01/database" >> ~/.bash_profile
-echo "" >> ~/.bash_profile
+echo "" >> $filePath
+echo "export ORACLE_HOSTNAME=ORCL" >> $filePath
+echo "export ORACLE_BASE=/u01/app/oracle" >> $filePath
+echo "export ORACLE_HOME=\$ORACLE_BASE/product/11.2.0/dbhome_1" >> $filePath
+echo "export ORACLE_SID=ORCL" >> $filePath
+echo "export PATH=.:\$ORACLE_HOME/bin:\$ORACLE_HOME/OPatch:\$ORACLE_HOME/jdk/bin:\$PATH" >> $filePath
+echo "export LC_ALL=\"en_US\"" >> $filePath
+echo "export LANG=\"en_US\"" >> $filePath
+echo "export NSL_LANG=\"AMERICAN_AMERICA.ZHS16GBK\"" >> $filePath
+echo "export NLS_DATE_FORMAT=\"YYYY-MM-DD HH24:MI:SS\"" >> $filePath
+echo "export DISTRIB=/u01/database" >> $filePath
+echo "" >> $filePath
 
-. ~/.bash_profile
+. $filePath
