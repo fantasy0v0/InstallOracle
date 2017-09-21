b8c2db66e4bc2ec48e9173a379f3db0f08a6cbcd
Fan <xiaofanms@outlook.com>
2017-09-21 21:44:17 +0800

    让修改tmpfs的配置生效。

--- add-swap.sh
+++ add-swap.sh
@@ -13,5 +13,7 @@ echo "/swap_4G swap swap defaults 0 0" >> /etc/fstab
 
 # 启动数据库时，shm不能小于等于MEMORY_TARGET配置的值
 echo "tmpfs /dev/shm tmpfs defaults,size=1G 0 0" >> /etc/fstab
+# 让上面的配置生效
+mount -o remount /dev/shm
 
 echo "Swap Completed."
